2156b59723fe292c4ed169e9b7ae60cc9f7981d7
SmitHacker <102592902+SmitDasadia@users.noreply.github.com>
2023-07-30 12:08:06 +0530

    Fix Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,16 +3,10 @@ import DarkModeToggle from "./DarkModeSwitch";
 
 const Footer = () => {
   return (
-    <footer className="text-gray-600 body-font dark:border-slate-600 dark:bg-dark-bg dark:text-dark-text border-t">
-      <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        
-        <p className="text-sm sm:ml-4 sm:pl-4   sm:py-2 sm:mt-0 mt-4">
-          © 2023 Notifyx
-          
-        </p>
-        <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-          <DarkModeToggle/>
-        </span>
+    <footer className=" py-4 px-8 text-gray-600 body-font dark:border-dark-bg dark:bg-dark-bg dark:text-dark-text border-t">
+      <div className="container mx-auto flex items-center justify-between">
+        <div className="text-sm">&copy; {new Date().getFullYear()} NotifyX</div>
+        <DarkModeToggle/>
       </div>
     </footer>
   );
